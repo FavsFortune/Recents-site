42387c2079806ca6580b21ae4ca120169bb9c85f
FavsFortune <141608441+FavsFortune@users.noreply.github.com>
2024-03-27 20:55:54 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+/*================================================typing animation==============================================*/
+var typed = new Typed(".typing",{
+    strings:["","Microbiologist","Web Designer","Fashion Designer","Graphic Designer","Data Analyst","Online Researcher","Virtual assistant","Content Writer", "Digital marketer", "Afflitate Marketer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
